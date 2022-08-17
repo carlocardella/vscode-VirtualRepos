9e13e471ca7d091f319c9915195cb54a10811c79
Carlo Cardella <carloc@microsoft.com>
2022-08-16 17:20:16 -0700

    Add GitHub authentication

--- src/extension.ts
+++ src/extension.ts
@@ -1,25 +1,27 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
+import { Credentials } from "./GitHub/authentication";
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
-	
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "vscode-Repos" is now active!');
+export async function activate(context: vscode.ExtensionContext) {
+    const credentials = new Credentials();
+    await credentials.initialize(context);
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('vscode-Repos.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Remote Repositories for Visual Studio Code!');
-	});
+    const disposable = vscode.commands.registerCommand(
+        "extension.getGitHubUser",
+        async () => {
+            const octokit = await credentials.getOctokit();
+            const userInfo = await octokit.users.getAuthenticated();
 
-	context.subscriptions.push(disposable);
+            vscode.window.showInformationMessage(
+                `Logged into GitHub as ${userInfo.data.login}`
+            );
+        }
+    );
+
+    context.subscriptions.push(disposable);
 }
 
 // this method is called when your extension is deactivated
