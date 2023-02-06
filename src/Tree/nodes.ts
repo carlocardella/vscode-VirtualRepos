d40e0f20bd5713b626a78c62f47b8266f203c080
Carlo Cardella <carloc@microsoft.com>
2023-02-06 07:22:07 -0800

    save progress

--- src/Tree/nodes.ts
+++ src/Tree/nodes.ts
@@ -240,9 +240,10 @@ export class RepoProvider implements TreeDataProvider<RepoNode | ContentNode>, T
         this._onDidChangeTreeData.fire(node);
     }
 
-    public getParent(element: RepoNode): RepoNode {
-        return this._getParent(element.name);
-    }
+    // public getParent(element: RepoNode): RepoNode {
+    //     return this._getParent(element.name);
+    // }
+
     // Drag and drop controller
 
     public async handleDrop(target: RepoNode | undefined, sources: DataTransfer, token: CancellationToken): Promise<void> {
@@ -250,16 +251,19 @@ export class RepoProvider implements TreeDataProvider<RepoNode | ContentNode>, T
         if (!transferItem) {
             return;
         }
-        const treeItems: RepoNode[] = transferItem.value;
-        let roots = this._getLocalRoots(treeItems);
-        // Remove nodes that are already target's parent nodes
-        roots = roots.filter((r) => !this._isChild(this._getTreeElement(r.key), target));
-        if (roots.length > 0) {
-            // Reload parents of the moving elements
-            const parents = roots.map((r) => this.getParent(r));
-            roots.forEach((r) => this._reparentNode(r, target));
-            this._onDidChangeTreeData.fire([...parents, target]);
-        }
+
+        const treeItems = store.repos!;
+
+        // const treeItems: RepoNode[] = transferItem.value;
+        // let roots = this._getLocalRoots(treeItems);
+        // // Remove nodes that are already target's parent nodes
+        // roots = roots.filter((r) => !this._isChild(this._getTreeElement(r.key), target));
+        // if (roots.length > 0) {
+        //     // Reload parents of the moving elements
+        //     const parents = roots.map((r) => this.getParent(r));
+        //     roots.forEach((r) => this._reparentNode(r, target));
+        //     this._onDidChangeTreeData.fire([...parents, target]);
+        // }
     }
 
     public async handleDrag(source: RepoNode[], treeDataTransfer: DataTransfer, token: CancellationToken): Promise<void> {
@@ -286,112 +290,112 @@ export class RepoProvider implements TreeDataProvider<RepoNode | ContentNode>, T
     }
 
     // From the given nodes, filter out all nodes who's parent is already in the the array of Nodes.
-    _getLocalRoots(nodes: RepoNode[]): RepoNode[] {
-        const localRoots = [];
-        for (let i = 0; i < nodes.length; i++) {
-            const parent = this.getParent(nodes[i]);
-            if (parent) {
-                const isInList = nodes.find((n) => n.name === parent.name);
-                if (isInList === undefined) {
-                    localRoots.push(nodes[i]);
-                }
-            } else {
-                localRoots.push(nodes[i]);
-            }
-        }
-        return localRoots;
-    }
+    // _getLocalRoots(nodes: RepoNode[]): RepoNode[] {
+    //     const localRoots = [];
+    //     for (let i = 0; i < nodes.length; i++) {
+    //         const parent = this.getParent(nodes[i]);
+    //         if (parent) {
+    //             const isInList = nodes.find((n) => n.name === parent.name);
+    //             if (isInList === undefined) {
+    //                 localRoots.push(nodes[i]);
+    //             }
+    //         } else {
+    //             localRoots.push(nodes[i]);
+    //         }
+    //     }
+    //     return localRoots;
+    // }
 
     // Remove node from current position and add node to new target element
-    _reparentNode(node: RepoNode, target: RepoNode | undefined): void {
-        const element: any = {};
-        element[node.name] = this._getTreeElement(node.name);
-        const elementCopy = { ...element };
-        this._removeNode(node);
-        const targetElement = this._getTreeElement(target?.name);
-        if (Object.keys(element).length === 0) {
-            targetElement[node.name] = {};
-        } else {
-            Object.assign(targetElement, elementCopy);
-        }
-    }
+    // _reparentNode(node: RepoNode, target: RepoNode | undefined): void {
+    //     const element: any = {};
+    //     element[node.name] = this._getTreeElement(node.name);
+    //     const elementCopy = { ...element };
+    //     this._removeNode(node);
+    //     const targetElement = this._getTreeElement(target?.name);
+    //     if (Object.keys(element).length === 0) {
+    //         targetElement[node.name] = {};
+    //     } else {
+    //         Object.assign(targetElement, elementCopy);
+    //     }
+    // }
 
     // Remove node from tree
-    _removeNode(element: RepoNode, tree?: any): void {
-        const subTree = tree ? tree : this.tree;
-        for (const prop in subTree) {
-            if (prop === element.name {
-                const parent = this.getParent(element);
-                if (parent) {
-                    const parentObject = this._getTreeElement(parent.name);
-                    delete parentObject[prop];
-                } else {
-                    delete this.tree[prop];
-                }
-            } else {
-                this._removeNode(element, subTree[prop]);
-            }
-        }
-    }
+    // _removeNode(element: RepoNode, tree?: any): void {
+    //     const subTree = tree ? tree : this.tree;
+    //     for (const prop in subTree) {
+    //         if (prop === element.name {
+    //             const parent = this.getParent(element);
+    //             if (parent) {
+    //                 const parentObject = this._getTreeElement(parent.name);
+    //                 delete parentObject[prop];
+    //             } else {
+    //                 delete this.tree[prop];
+    //             }
+    //         } else {
+    //             this._removeNode(element, subTree[prop]);
+    //         }
+    //     }
+    // }
 
-    _getChildren(key: string | undefined): string[] {
-        if (!key) {
-            return Object.keys(this.tree);
-        }
-        const treeElement = this._getTreeElement(key);
-        if (treeElement) {
-            return Object.keys(treeElement);
-        }
-        return [];
-    }
+    // _getChildren(key: string | undefined): string[] {
+    //     if (!key) {
+    //         return Object.keys(this.tree);
+    //     }
+    //     const treeElement = this._getTreeElement(key);
+    //     if (treeElement) {
+    //         return Object.keys(treeElement);
+    //     }
+    //     return [];
+    // }
 
-    _getTreeItem(key: string): TreeItem {
-        const treeElement = this._getTreeElement(key);
-        // An example of how to use codicons in a MarkdownString in a tree item tooltip.
-        const tooltip = new MarkdownString(`$(zap) Tooltip for ${key}`, true);
-        return {
-            label: /**TreeItemLabel**/ <any>{ label: key, highlights: key.length > 1 ? [[key.length - 2, key.length - 1]] : void 0 },
-            tooltip,
-            collapsibleState: treeElement && Object.keys(treeElement).length ? TreeItemCollapsibleState.Collapsed : TreeItemCollapsibleState.None,
-            resourceUri: Uri.parse(`/tmp/${key}`),
-        };
-    }
+    // _getTreeItem(key: string): TreeItem {
+    //     const treeElement = this._getTreeElement(key);
+    //     // An example of how to use codicons in a MarkdownString in a tree item tooltip.
+    //     const tooltip = new MarkdownString(`$(zap) Tooltip for ${key}`, true);
+    //     return {
+    //         label: /**TreeItemLabel**/ <any>{ label: key, highlights: key.length > 1 ? [[key.length - 2, key.length - 1]] : void 0 },
+    //         tooltip,
+    //         collapsibleState: treeElement && Object.keys(treeElement).length ? TreeItemCollapsibleState.Collapsed : TreeItemCollapsibleState.None,
+    //         resourceUri: Uri.parse(`/tmp/${key}`),
+    //     };
+    // }
 
-    _getTreeElement(element: string | undefined, tree?: any): any {
-        if (!element) {
-            return this.tree;
-        }
-        const currentNode = tree ?? this.tree;
-        for (const prop in currentNode) {
-            if (prop === element) {
-                return currentNode[prop];
-            } else {
-                const treeElement = this._getTreeElement(element, currentNode[prop]);
-                if (treeElement) {
-                    return treeElement;
-                }
-            }
-        }
-    }
+    // _getTreeElement(element: string | undefined, tree?: any): any {
+    //     if (!element) {
+    //         return this.tree;
+    //     }
+    //     const currentNode = tree ?? this.tree;
+    //     for (const prop in currentNode) {
+    //         if (prop === element) {
+    //             return currentNode[prop];
+    //         } else {
+    //             const treeElement = this._getTreeElement(element, currentNode[prop]);
+    //             if (treeElement) {
+    //                 return treeElement;
+    //             }
+    //         }
+    //     }
+    // }
 
-    _getParent(element: string, parent?: string, tree?: any): any {
-        const currentNode = tree ?? this.tree;
-        for (const prop in currentNode) {
-            if (prop === element && parent) {
-                return this._getNode(parent);
-            } else {
-                const parent = this._getParent(element, prop, currentNode[prop]);
-                if (parent) {
-                    return parent;
-                }
-            }
-        }
-    }
+    // _getParent(element: string, parent?: string, tree?: any): any {
+    //     const currentNode = tree ?? this.tree;
+    //     for (const prop in currentNode) {
+    //         if (prop === element && parent) {
+    //             return this._getNode(parent);
+    //         } else {
+    //             const parent = this._getParent(element, prop, currentNode[prop]);
+    //             if (parent) {
+    //                 return parent;
+    //             }
+    //         }
+    //     }
+    // }
 
-    _getNode(key: string): RepoNode {
-        if (!this.nodes[key]) {
-            this.nodes[key] = new Key(key);
-        }
-        return this.nodes[key];
-    }
+    // _getNode(key: string): RepoNode {
+    //     if (!this.nodes[key]) {
+    //         this.nodes[key] = new Key(key);
+    //     }
+    //     return this.nodes[key];
+    // }
 }
